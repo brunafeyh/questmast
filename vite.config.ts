baa97b2eeacc802263cb0d29d1e2dacbf621a903
Bruna Feyh <bruna.feyh@unioeste.br>
2025-03-09 14:31:48 -0300

    feat: define base build

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://brunafeyh.github.io/questmast/",
+  build: {
+    outDir: 'build'
+  }
 })
+
